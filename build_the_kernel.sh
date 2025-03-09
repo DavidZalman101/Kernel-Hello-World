9dd768fc0e1c55653c66905bfb219e5cf118feb7
DavidZalman101 <davidzalman.101@gmail.com>
2025-03-09 16:14:02 +0200

    Build and run
    Add bash scripts to build the kernel and run it using qemu

---
+++ build_the_kernel.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# run the assembler to create the object file kasm.o in ELF-32 bit format
+nasm -f elf32 kernel.asm -o kasm.o
+
+# Compile the kernel.c, but dont link
+gcc -m32 -c kernel.c -o kc.o
+
+# run the linker with our linker script
+ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
